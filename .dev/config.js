0adb81bf61e689ab5a2a7bb77420a4864ee47ec7
vdm <exgart.mbf@gmail.com>
2022-05-13 13:58:12 +0300

    add const GITHUB_URL in config.js

--- .dev/config.js
+++ .dev/config.js
@@ -1,3 +1,4 @@
+const GITHUB_URL = 'https://github.com/mb-exec/fractales.dev.git'
 // const SERVER_URL = 'http://'
 const SERVER_BASE_URL = 'http://localhost:8888'
 
@@ -6,4 +7,7 @@ const ROOT_FOLDER_NAME = path.resolve(__dirname, '../').split('/').pop()
 
 const SERVER_URL = SERVER_BASE_URL + '/' + ROOT_FOLDER_NAME
 
-module.exports = SERVER_URL
+module.exports = {
+  serverUrl: SERVER_URL,
+  gitUrl: GITHUB_URL
+}
