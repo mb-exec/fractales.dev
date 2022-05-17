96e8549b225a28a7af504e89582c90c118cac08a
vdm <exgart.mbf@gmail.com>
2022-05-17 11:44:19 +0300

    add local db
    local php files with information for project

---
+++ app/db/_pages.php
@@ -0,0 +1,66 @@
+<?php
+  $pages = [
+    // скрипты и стили для всех страниц
+    'global' => [
+      'scripts' => [
+        'main' => '1.0.0',
+      ],
+
+      'styles' => [
+        'style' => '1.0.0',
+      ]
+     ],
+
+    // настройки для каждой страницы
+    'index' => [
+      'title' => 'Fractales - index',
+      'description' => '',
+      'scripts' => [
+        'swiper-bundle.min' => '1.0.0', 
+        'index-page' => '1.0.0',
+      ],
+      'styles' => [
+        'index-page' => '1.0.0'
+      ],
+    ],
+
+    'about' => [
+      'title' => 'Fractales - about',
+      'description' => '',
+      'scripts' => [
+        'about' =>  '1.0.0',
+      ],
+      'styles' => [
+        'about' =>  '1.0.0',
+      ],
+    ],
+    
+    'works' => [
+      'title' => 'Fractales - works',
+      'description' => '',
+      'scripts' => [],
+      'styles' => [
+        'works' =>  '1.0.0',
+      ],
+    ],
+
+    'fuenf' => [
+      'title' => 'Fractales - fuenf',
+      'description' => '',
+      'scripts' => [],
+      'styles' => [
+        'case' =>  '1.0.0',
+        'fuenf' =>  '1.0.0'
+      ],
+    ],
+
+    'demetra' => [
+      'title' => 'Fractales - demetra',
+      'description' => '',
+      'scripts' => [],
+      'styles' => [
+        'case' =>  '1.0.0',
+      ],
+    ],
+  ]
+?>
---
+++ app/db/index.php
@@ -0,0 +1,12 @@
+<?php 
+require '_contacts.php';
+require '_pages.php';
+require '_cases.php';
+
+$db = [
+  'cases' => $cases,
+  'pages' => $pages,
+  'contacts' => $contacts,
+]
+
+?>
