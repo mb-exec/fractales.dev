aaa399cef6ed214a990860f3c699895f79ec3246
vdm <exgart.mbf@gmail.com>
2022-05-17 10:07:58 +0300

    deleting unused code from gitDeploy

--- .dev/gitDeploy.js
+++ .dev/gitDeploy.js
@@ -10,26 +10,17 @@ function deployBuild(cb) {
   git.checkIsRepo('root')
     .then(isRepo => {
       if (!isRepo) {
-        console.log('as');
         git.init()
           .then(() => git.addRemote('origin', gitSSH))
           .then(() => git.checkout(['-b', 'prod']))
       }
     })
     .then(() => {
-      git
-        // .status((e, s) => console.log(s))
-        .add('./*')
+      git.add('./*')
         .commit('upd production build')
         .push(['-u', 'origin', 'prod', '--force'])
     })
     .finally(() => cb()) 
-}
-
-function initialiseRepo (git) {
-  return git.init()
-     .then(() => git.addRemote('origin', gitSSH))
-     .then(() => git.checkout(['-b', 'prod']))
 }
 
 module.exports = deployBuild
