c6949f62a7eaa6bdf08411391aa20d32130ced4e
vdm <exgart.mbf@gmail.com>
2022-05-17 12:10:18 +0300

    init index page

---
+++ app/templates/parts/index/index-services.php
@@ -0,0 +1,69 @@
+<?php
+  $services = [
+    [
+      'title' => 'Websites development',
+      'descr' => 'We create websites that highlight the brand identity, bring customers and enhance the corporate image of the company.',
+      'list' => [
+        'Corporate website',
+        'Online shopping',
+        'Landing Pages'
+      ]
+    ],
+    [
+      'title' => 'Design and branding',
+      'descr' => 'We create a holistic image of the company that reflects its values and distinguishes it from competitors. We convey brand ideas to the consumer through metaphors.',
+      'list' => [
+        'Corporate identity',
+        'Graphic design',
+        'Brand packaging'
+      ]
+    ],
+    [
+      'title' => 'Web services',
+      'descr' => 'We develop technological solutions for non-standard tasks: portals, services, personal accounts. We explore JTBD and CJM and understand what drives users.',
+      'list' => [
+        'Dashboards',
+        'Services',
+        'Personal accounts'
+      ]
+    ],
+    [
+      'title' => 'UX/UI design',
+      'descr' => 'We develop technological solutions for non-standard tasks: portals, services, personal accounts. We explore JTBD and CJM and understand what drives users.',
+      'list' => [
+        'UX reserch',
+        'Prototype',
+        'Design concept'
+      ]
+    ],
+  ]
+?>
+
+<section id="services" class="services sect">
+  <div class="container">
+    <h2 class="sect__title">What we do?</h2>
+
+    <ol class="services__list">
+      <?php 
+        $i = 0;
+        foreach ( $services as $s ) : 
+        $animation_direction = $i % 2 ? 'appear-left' : 'appear-right';
+      ?>
+        <li class="service text-ls" animation="<?php echo $animation_direction ?>">
+          <h3 class="service__title"><?php echo $s['title'] ?></h3>
+          <div class="service__bottom">            
+            <p class="service__descr"><?php echo $s['descr'] ?></p>
+            <ul class="service__list">
+              <?php foreach ( $s['list'] as $item ) : ?>
+                <li class="service__list-item"><?php echo $item ?></li>
+              <?php endforeach ?>
+            </ul>
+          </div>
+        </li>
+      <?php 
+        $i++;
+        endforeach 
+      ?>
+    </ol>
+  </div>
+</section>
--- index.php
+++ index.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php require 'app/templates/header.php' ?>
+
+<section class="hero">
+  <div class="container">
+    <div class="hero__inner">
+      <h1 class="hero__title" animation="appear-left">We focus on creating<br>a quality digital product for your business</h1>
+      <div class="hero__bottom" animation="appear-right">        
+        <p class="hero__text text-ls">Let's create effective website or unique brand identity with attention to detail together</p>
+        <a href="#contacts" class="hero__link btn text-ls frame-hover">
+          Start project <span class="small-arrow"></span>
+          <span class="frame-hover__top"></span>
+          <span class="frame-hover__bottom"></span>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+require 'app/templates/parts/index/index-services.php';
+require 'app/templates/parts/index/index-cases.php';
+require 'app/templates/parts/index/index-technology.php';
+require 'app/templates/parts/index/index-about.php';
+?>
+
+<?php require 'app/templates/footer.php' ?>
