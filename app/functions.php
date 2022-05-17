977677800f2b0b6531ff35c988f84d9510d29438
vdm <exgart.mbf@gmail.com>
2022-05-17 11:50:20 +0300

    add header & footer & functions.php

---
+++ app/functions.php
@@ -0,0 +1,15 @@
+<?php
+require 'inc/index.php';
+require 'db/index.php';
+
+$site_url = get_site_url();
+
+$curr_page_name = get_page_template();
+$page = $db['pages'][$curr_page_name];
+
+$tel = $db['contacts']['tel'];
+$tel_link = str_replace(' ', '', $tel);
+$address = $db['contacts']['address'];
+$email = $db['contacts']['email'];
+$socials = $db['contacts']['socials'];
+?>
