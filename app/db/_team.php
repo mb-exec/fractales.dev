5b5d77f3701ed9a3f89897d0728484f6a5a5470e
vdm <exgart.mbf@gmail.com>
2022-05-17 12:44:58 +0300

    add team db info

---
+++ app/db/_team.php
@@ -0,0 +1,36 @@
+<?php
+$team_photo_dir = '/assets/img/team/';
+
+$team = [
+  [
+    'name' => 'Andrey',
+    'position' => 'Executive Director',
+    'photo' => $team_photo_dir . 'Andrey',
+  ],
+  [
+    'name' => 'Alexandra',
+    'position' => 'Director of Sales',
+    'photo' => $team_photo_dir . 'Alexandra',
+  ],
+  [
+    'name' => 'Sofia',
+    'position' => 'UX/UI Designer',
+    'photo' => $team_photo_dir . 'Sofia',
+  ],
+  [
+    'name' => 'Alexandra',
+    'position' => 'Director of Sales',
+    'photo' => $team_photo_dir . 't1',
+  ],
+  [
+    'name' => 'Sofia',
+    'position' => 'UX/UI Designer',
+    'photo' => $team_photo_dir . 't2',
+  ],
+  [
+    'name' => 'Alexandra',
+    'position' => 'Director of Sales',
+    'photo' => $team_photo_dir . 't3',
+  ],
+];
+?>
--- app/db/index.php
+++ app/db/index.php
@@ -2,11 +2,13 @@
 require '_contacts.php';
 require '_pages.php';
 require '_cases.php';
+require '_team.php';
 
 $db = [
   'cases' => $cases,
   'pages' => $pages,
   'contacts' => $contacts,
+  'team' => $team,
 ]
 
 ?>
