1ca060c416aae2b16e7281e33d9cae7b927bd404
vdm <exgart.mbf@gmail.com>
2022-05-18 10:20:12 +0300

    add works page

---
+++ works.php
@@ -0,0 +1,19 @@
+<?php require 'app/templates/header.php' ?>
+
+<section class="works">
+  <div class="container">
+    <h1 class="works__title title-lg" animation="appear-left">Works</h1>
+
+    <ul class="works__list">
+      <?php 
+        $i = 0;
+        foreach ( $cases as $name => $case ) {
+          echo get_case_card( $case, $i );
+          $i++;
+        }
+      ?>
+    </ul>
+  </div>
+</section>
+
+<?php require 'app/templates/footer.php' ?>
