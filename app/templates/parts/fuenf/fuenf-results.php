68dbb6431252b976603f09a58b0ec6cb527af454
vdm <exgart.mbf@gmail.com>
2022-05-18 10:52:02 +0300

    add fuenf results template & fuenf styles

---
+++ app/templates/parts/fuenf/fuenf-results.php
@@ -0,0 +1,16 @@
+<section class="results sect sect--columns">
+  <div class="container">
+
+    <div class="results__inner sect--columns__inner">
+      <h2 class="case__sect-title sect--columns__inner-title">RESULT</h2>
+
+      <div class="sect--columns__inner-content text-ls">
+        <ul class="results__list">
+          <li class="results__list-item"><b>3</b>Click to buy</li>
+          <li class="results__list-item"><b>2</b>Payment methods</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</section>
