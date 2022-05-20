52e3c816b8d437b42b4a90c3086a08e21716549d
vdm <exgart.mbf@gmail.com>
2022-05-20 12:32:57 +0300

    init manifest case

---
+++ app/db/single-cases/manifest.php
@@ -0,0 +1,47 @@
+<?php
+  $manifest = [
+    'title' => 'Digital media magazine for modern women',
+    'tag' => 'Services',
+
+    'card_descr' => 'Together with the customer, we transformed the Instagram account into a large digital resource with well-thought-out section navigation.',
+
+    'imgs_folder' => $site_url . '/assets/img/cases/manifest/',
+    'card_img_alt' => 'Manifest case preview',
+
+    'page_link' => $site_url . '/cases/manifest',
+
+    // для секции с основной информацией
+    'descr' => '',
+
+    'services' => [
+      'Web services',
+      'UX/UI design',
+    ],
+    'technology' => [
+      'CSS 3',
+      'HTML 5',
+      'WordPress',
+    ],
+
+    // секция about
+    'about' => '',
+
+    // секция design
+    'design' => [
+      'strong' => '',
+      'text' => ""
+    ],
+
+    // изображения для секций
+    'imgs' => [
+      'header' => [
+        'name' => 'hdr',
+        'alt' => 'Site preview on laptop'
+      ],
+      'about' => [
+        'name' => 'about',
+        'alt' => 'Site preview on mobile'
+      ]
+    ]
+  ]
+?>
