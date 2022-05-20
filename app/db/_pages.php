f1e31f401fd732744f01a6ade0d33a009bbb1d77
vdm <exgart.mbf@gmail.com>
2022-05-20 16:48:04 +0300

    change pages titles & descriptions

--- app/db/_pages.php
+++ app/db/_pages.php
@@ -13,8 +13,8 @@
 
     // настройки для каждой страницы
     'index' => [
-      'title' => 'Fractales - web development company',
-      'description' => 'Custom web app development company with offices in Estonia, USA, and Norway.',
+      'title' => 'Custom Web Design & Web Development | fractales dev',
+      'description' => 'Our team of IT specialists create quality digital products that emphasize the individuality of your business, bring customers and enhance the corporate image of the company. We specialize in website and web service development, design and branding.',
       'scripts' => [
         'swiper-bundle.min' => '1.0.0', 
         'index-page' => '1.0.0',
@@ -25,8 +25,8 @@
     ],
 
     'about' => [
-      'title' => 'Fractales - about us',
-      'description' => '',
+      'title' => 'Our team of specialists | fractales dev',
+      'description' => 'We believe that the team is the most valuable asset in any business. Each of us is a professional in his field. This helps to separate tasks and work efficiently.',
       'scripts' => [
         'about' =>  '1.0.0',
       ],
@@ -36,7 +36,7 @@
     ],
     
     'works' => [
-      'title' => 'Fractales - our works',
+      'title' => 'Our works | fractales dev ',
       'description' => '',
       'scripts' => [],
       'styles' => [
