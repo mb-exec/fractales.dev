de42e601c9c5cc29cb6d00bdc0554c95a92dae57
vdm <exgart.mbf@gmail.com>
2022-05-31 10:50:33 +0300

    update pic in about-hero

--- about.php
+++ about.php
@@ -5,11 +5,14 @@
     <h1 class="title-lg">fractales dev</h1>
     <p class="about-hero__descr text-ls">Focusing on individual details, we create a high-quality holistic product</p>
     <picture class="about-hero__pic">
-      <source media="(min-width: 767.98px)" srcset="<?php echo $site_url ?>/assets/img/about-page/hero-lg.webp" type="image/webp">
-      <source media="(min-width: 767.98px)" srcset="<?php echo $site_url ?>/assets/img/about-page/hero-lg.jpg" type="image/jpeg">
 
-      <source media="(max-width: 767.98px)" srcset="<?php echo $site_url ?>/assets/img/about-page/hero-xs.webp" type="image/webp">
-      <img loading="lazy" src="<?php echo $site_url ?>/assets/img/about-page/hero-xs.jpg" alt="#" class="about-hero__img">
+      <?php if ($webp_support) : ?>
+        <source media="(min-width: 767.98px)" srcset="<?php echo $site_url ?>/assets/img/about-page/hero-lg.webp" type="image/webp">
+        <img loading="lazy" src="<?php echo $site_url ?>/assets/img/about-page/hero-xs.webp" alt="#" class="about-hero__img">
+      <?php else : ?>
+        <source media="(min-width: 767.98px)" srcset="<?php echo $site_url ?>/assets/img/about-page/hero-lg.jpg" type="image/jpeg">
+        <img loading="lazy" src="<?php echo $site_url ?>/assets/img/about-page/hero-xs.jpg" alt="#" class="about-hero__img">      
+      <?php endif ?>
     </picture>
   </div>
 </section>
