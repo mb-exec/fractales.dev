abbed06a7df2ff6cbf726f1ab0fe40b4214f3e81
vdm <exgart.mbf@gmail.com>
2022-06-22 11:35:19 +0300

    format functions.php

--- app/functions.php
+++ app/functions.php
@@ -3,8 +3,9 @@ define('__ROOT__', dirname(__DIR__, 1));
 
 require __ROOT__ . '/app/inc/index.php';
 
-$site_url = get_site_url();
+$site_url       = get_site_url();
 $curr_page_name = get_page_template();
+
 $webp_support = 
   strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false || 
   strpos( $_SERVER['HTTP_USER_AGENT'], ' Chrome/' ) !== false;
@@ -14,9 +15,9 @@ require __ROOT__ . '/app/components/index.php';
 
 $page = $db['pages'][$curr_page_name];
 
-$tel = $db['contacts']['tel'];
-$tel_link = str_replace(' ', '', $tel);
+$tel       = $db['contacts']['tel'];
+$tel_link  = str_replace(' ', '', $tel);
 $addresses = $db['contacts']['addresses'];
-$email = $db['contacts']['email'];
-$socials = $db['contacts']['socials'];
+$email     = $db['contacts']['email'];
+$socials   = $db['contacts']['socials'];
 ?>
